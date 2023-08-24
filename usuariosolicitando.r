f7f228a20214038dbc60aa03865dcde3caa08be6
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-08-24 11:43:05 -0300

    comentario
    fiz apenas um comentario pois tinha esquecido de baixar o python studo e alguns pacotes do R

--- usuariosolicitando.r
+++ usuariosolicitando.r
@@ -3,3 +3,5 @@ saudacao = readline(prompt = "Indique a saudação : ");
 pessoa = readline(prompt = "Indique o nome da pessoa saudada: ");
 
 cat(saudacao,",",pessoa,"!")
+
+#pra funcionar direito fiz o download necessario dos pacotes, tinha esquecido
